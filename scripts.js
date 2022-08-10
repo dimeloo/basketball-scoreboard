5f6ed6b1b8d2ae03e4fe4ac3ad9690e3711993d9
Mike Ortiz <dimelo@Mikes-MacBook-Air.local>
2022-08-10 00:52:03 -0400

    basketball-scoreboard

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let homeEl = document.getElementById('home-el')
+let guestEl = document.getElementById('guest-el')
+
+let addingOne = 1
+let addingTwo = 2
+let addingThree = 3
+
+function addOneHome() {
+    homeEl.innerHTML += 1
+    result = homeEl.innerHTML
+    console.log(result)
+}
